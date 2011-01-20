917684c3ff1259f6cc1ca3b86e9252153a9ebdd2
dzamkov <dzamkov@xiatek.org>
2011-01-19 20:53:31 -0500

    Project started

---
+++ Manifold.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CultureInfo = System.Globalization.CultureInfo;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace HC
+{
+    /// <summary>
+    /// A fancy name for "level", or "map".
+    /// </summary>
+    public class Manifold
+    {
+        private Manifold()
+        {
+
+        }
+
+        /// <summary>
+        /// Loads (and smooths) a manifold from the individual lines of a OBJ file.
+        /// </summary>
+        public static Manifold Load(string[] Lines)
+        {
+            List<Vector> verts = new List<Vector>();
+            List<List<int>> faces = new List<List<int>>();
+
+            foreach (string line in Lines)
+            {
+                if (line != "")
+                {
+                    string[] parts = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (parts[0] == "v")
+                    {
+                        verts.Add(new Vector(
+                            double.Parse(parts[1], CultureInfo.InvariantCulture),
+                            double.Parse(parts[2], CultureInfo.InvariantCulture),
+                            double.Parse(parts[3], CultureInfo.InvariantCulture)));
+                    }
+                    if (parts[0] == "f")
+                    {
+                        List<int> vs = new List<int>();
+                        for(int t = 1; t < parts.Length; t++)
+                        {
+                            vs.Add(int.Parse(parts[t]) - 1);
+                        }
+                        faces.Add(vs);
+                    }
+                }
+            }
+
+            Manifold mf = new Manifold();
+            mf._Vertices = verts;
+            mf._Triangles = new List<Triangle<int>>();
+            mf._Segments = new Dictionary<Segment<int>, Triangle<int>>();
+            foreach (List<int> face in faces)
+            {
+                int first = face[0];
+                for (int i = 2; i < face.Count; i++)
+                {
+                    mf._AddTriangle(new Triangle<int>(first, face[i - 1], face[i]));
+                }
+            }
+
+            return mf;
+        }
+
+        /// <summary>
+        /// Adds a triangle, along with segment references.
+        /// </summary>
+        private void _AddTriangle(Triangle<int> Tri)
+        {
+            this._Triangles.Add(Tri);
+            foreach (Segment<int> seg in Tri.Segments)
+            {
+                this._Segments.Add(seg, Tri);
+            }
+        }
+
+        /// <summary>
+        /// Renders the manifold to the current graphics context.
+        /// </summary>
+        public void Render()
+        {
+            GL.Begin(BeginMode.Triangles);
+            foreach (Triangle<int> tri in this._Triangles)
+            {
+                GL.Vertex3(this._Vertices[tri.A]);
+                GL.Vertex3(this._Vertices[tri.B]);
+                GL.Vertex3(this._Vertices[tri.C]);
+            }
+            GL.End();
+        }
+
+        /// <summary>
+        /// Loads a manifold from the specified file path.
+        /// </summary>
+        public static Manifold Load(string Path)
+        {
+            return Load(File.ReadAllLines(Path));
+        }
+
+        private List<Vector> _Vertices;
+        private List<Triangle<int>> _Triangles;
+        private Dictionary<Segment<int>, Triangle<int>> _Segments;
+    }
+}
---
+++ Window.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace HC
+{
+    /// <summary>
+    /// Main program window.
+    /// </summary>
+    public class Window : GameWindow
+    {
+        public Window() : base(640, 480, GraphicsMode.Default, "HyperCycles!")
+        {
+            this.WindowBorder = WindowBorder.Resizable;
+            this.WindowState = WindowState.Maximized;
+
+            this._Manifold = Manifold.Load("../../Maps/1.obj");
+
+            GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
+        }
+
+        /// <summary>
+        /// Program main entry point.
+        /// </summary>
+        public static void Main(string[] Args)
+        {
+            new Window().Run(60.0);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.ClearColor(Color.RGB(0.0, 0.0, 0.0));
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.MatrixMode(MatrixMode.Projection);
+            Matrix4d proj = Matrix4d.CreatePerspectiveFieldOfView(0.7, (double)this.Width / (double)this.Height, 0.1, 100.0);
+            GL.LoadMatrix(ref proj);
+            Matrix4d view = Matrix4d.LookAt(3.0 * Math.Sin(this._Time), 3.0 * Math.Cos(this._Time), 3.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
+            GL.MultMatrix(ref view);
+
+            this._Manifold.Render();
+
+            this.SwapBuffers();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            this._Time += e.Time;
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, this.Width, this.Height);
+        }
+
+        private double _Time;
+        private Manifold _Manifold;
+    }
+}
